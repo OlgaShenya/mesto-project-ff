db8bdb39f766c7fe1591adb35bfb521fbe983ecd
Olga Shishenya <olga.shenya@gmail.com>
2024-01-30 23:07:04 +0300

    Opening and Closing Modals

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,1 +1,19 @@
 //Работа модальных окон (openModal, closeModal)
+
+function escHandler(evt) {
+  if (evt.key === "Escape") {
+    closeModal();
+  }
+}
+
+export function showModal(modal) {
+  modal.classList.add("popup_is-opened");
+  window.addEventListener("keydown", escHandler);
+}
+
+export function closeModal() {
+  const modal = document.querySelector(".popup_is-opened");
+  modal.classList.add("popup_is-animated");
+  modal.classList.remove("popup_is-opened");
+  window.removeEventListener("keydown", escHandler);
+}
--- src/index.js
+++ src/index.js
@@ -6,11 +6,39 @@
 import "./styles/index.css"; // добавьте импорт главного файла стилей
 import initialCards from "./components/cards";
 import { createCard, deleteCard } from "./components/card";
+import { showModal, closeModal } from "./components/modal";
+
 const placesList = document.querySelector(".places__list");
+const editProfileButton = document.querySelector(".profile__edit-button");
+const addPlaceButton = document.querySelector(".profile__add-button");
+const popupEdit = document.querySelector(".popup_type_edit");
+const popupNewCard = document.querySelector(".popup_type_new-card");
+const popupImage = document.querySelector(".popup_type_image");
 
 window.addEventListener("load", () => {
   initialCards.forEach((card) => {
     const cardElement = createCard(card, deleteCard);
     placesList.append(cardElement);
   });
+});
+
+editProfileButton.addEventListener("click", () => {
+  showModal(popupEdit);
+});
+
+addPlaceButton.addEventListener("click", () => {
+  showModal(popupNewCard);
+});
+
+placesList.addEventListener("click", ({ target }) => {
+  if (target.classList.contains("card__image")) {
+    document.querySelector(".popup__image").src = target.src;
+    showModal(popupImage);
+  }
+});
+
+window.addEventListener("click", (evt) => {
+  if (evt.target.classList.contains("popup__close") || evt.target.classList.contains("popup")) {
+    closeModal();
+  }
 });
